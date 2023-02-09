4b4e4c42646bf8bd35f7e3b11f24fef5eae3b7d2
zm1hawk <vinicius6.lopes6@gmail.com>
2023-02-09 16:40:44 -0300

    Pilha de chamadas de métodos (stack trace)

--- src/application/Program_Exception.java
+++ src/application/Program_Exception.java
@@ -7,7 +7,24 @@ public class Program_Exception {
 
 	public static void main(String[] args) {
 		
+		method1();
+		
+		System.out.println("End of program");
+		
+
+	}
+	public static void method1() {
+		System.out.println("---METHOD1 START---");
+		method2();
+		System.out.println("---METHOD1 END---");
+	}
+	
+	
+	
+	public static void method2() {
+		System.out.println("---METHOD2 START---");
 		Scanner leia = new Scanner (System.in);
+		
 		try{
 		
 		String[] vect = leia.nextLine().split(" ");
@@ -17,15 +34,15 @@ public class Program_Exception {
 		}
 		catch(ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position!");
+			e.printStackTrace();
+			leia.next();
 		}
 		catch(InputMismatchException e) {
 			System.out.println("Input Error");
+			
 		}
 		
-		System.out.println("End of program");
-		
 		leia.close();
-
+		System.out.println("---METHOD2 END---");
 	}
-
 }
