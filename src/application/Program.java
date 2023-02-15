8d241d923839db28ad5221beaf0bd54c0078bba9
zm1hawk <vinicius6.lopes6@gmail.com>
2023-02-15 09:25:52 -0300

    FileWriter e BufferedWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,31 +1,26 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		String path = "c:\\temp\\in.txt";
+		String[] lines = new String[] {"Good morning","Good afternoon", "Good night"};
 		
+		String path = "c:\\temp\\out.txt";
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-			
-			String line = br.readLine();
-			
-			while(line != null) {
-				System.out.println(line);
-				line = br.readLine();
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
-		
 		}
-		catch(IOException e){
-			System.out.println("Error: "+e.getMessage());
+		catch(IOException e) {
+			e.printStackTrace();
 		}
-		
 		
 	}
-
 }
