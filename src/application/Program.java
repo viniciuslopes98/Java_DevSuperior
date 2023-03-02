2d35a2cbc011ca583a0db46d00e83eb82dc673d9
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-02 14:00:20 -0300

    Informações do caminho do arquivo

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,24 +9,14 @@ public class Program {
 
 		Scanner leia = new Scanner(System.in);
 
-		System.out.println("Enter a folder path: ");
+		System.out.println("Enter a file path: ");
 		String strPath = leia.nextLine();
-
+		
 		File path = new File(strPath);
-
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS: ");
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("FILES: ");
-		for (File file : files) {
-			System.out.println(file);
-		}
-
-		boolean sucess = new File(strPath + "\\SubPasta").mkdir();
-		System.out.println("Directory created successfully: " + sucess);
+		
+		System.out.println("getName: "+path.getName());
+		System.out.println("getParent: "+path.getParent());
+		System.out.println("getPath: "+path.getPath());
 
 		leia.close();
 
