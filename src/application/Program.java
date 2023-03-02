ff2bc67cbd0e74c4710c7dd82d02ca7bf082b297
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-02 13:18:08 -0300

    $ git commit -m

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,26 +1,34 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		String[] lines = new String[] {"Good morning","Good afternoon", "Good night"};
-		
-		String path = "c:\\temp\\out.txt";
-		
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
-			for(String line : lines) {
-				bw.write(line);
-				bw.newLine();
-			}
+		Scanner leia = new Scanner(System.in);
+
+		System.out.println("Enter a folder path: ");
+		String strPath = leia.nextLine();
+
+		File path = new File(strPath);
+
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS: ");
+		for (File folder : folders) {
+			System.out.println(folder);
 		}
-		catch(IOException e) {
-			e.printStackTrace();
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES: ");
+		for (File file : files) {
+			System.out.println(file);
 		}
-		
+
+		boolean sucess = new File(strPath + "\\SubPasta").mkdir();
+		System.out.println("Directory created successfully: " + sucess);
+
+		leia.close();
+
 	}
 }
