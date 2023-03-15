9c606477b4a0bd850ae5400abebc57cf70a22eac
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-14 22:08:31 -0300

    Exemplo de Curingas delimitados (bounded

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,33 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+//    Fazer um método que copia os elementos de uma lista para uma
+//    outra lista que pode ser mais genérica que a primeira.
+
 public class Program {
-
-//  Princípio get/put - Contravariância:
-
 	public static void main(String[] args) {
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
 		List<Object> myObjs = new ArrayList<Object>();
-		myObjs.add("Maria");
-		myObjs.add("Alex");
-		List<? super Number> myNums = myObjs;
-		myNums.add(10);
-		myNums.add(3.14);
-//		Number x = myNums.get(0);  erro de compilação
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
+	}
 
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for (Number number : source) {
+			destiny.add(number);
+		}
+	}
+
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
 	}
 }
