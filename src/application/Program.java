8900adbe680a76b6d9736ad917e506e34e38b644
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-15 08:43:28 -0300

    Set - Teste de igualdade

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,48 +1,21 @@
 package application;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
+
+import entities.Product_Set;
 
 public class Program {
+
 	public static void main(String[] args) {
+		Set<Product_Set> set = new HashSet<>();
 
-		Set<String> set = new LinkedHashSet<>();
+		set.add(new Product_Set("TV", 900.0));
+		set.add(new Product_Set("Notebook", 1200.0));
+		set.add(new Product_Set("Tablet", 400.0));
 
-		set.add("TV");
-		set.add("Notebook");
-		set.add("Tablet");
-		set.add("Computador");
-		set.add("Celular");
-		set.add("Fone");
+		Product_Set prod = new Product_Set("Notebook", 1200.0);
 
-		set.remove("Tablet");
-		set.removeIf(x -> x.length() <= 2); //Remove todos os itens com a quantia de caracteres menor/igual a 2.
-		set.removeIf(x -> x.charAt(0) == 'N');// Remove todos os itens que se inicia com o caracter 'N'.
-
-		for (String p : set) {
-			System.out.println(p);
-		}
-		
-		System.out.println();
-		
-		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
-		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
-		
-		// union
-		Set<Integer> c = new TreeSet<>(a);
-		c.addAll(b);
-		System.out.println("União: "+ c);
-		
-		// intersection
-		Set<Integer> d = new TreeSet<>(a);
-		d.retainAll(b);
-		System.out.println("Intersecção: "+d);
-		
-		// difference
-		Set<Integer> e = new TreeSet<>(a);
-		e.removeAll(b);
-		System.out.println("Diferença: "+e);
+		System.out.println(set.contains(prod));//Precisa implementar Hashcode & Equals na classe Product_Set. 
 	}
 }
