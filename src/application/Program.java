544781724191d3ebb69aa246d77151c03545ee3e
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-15 08:35:17 -0300

    Set/Métodos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,33 +1,48 @@
 package application;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-
-//    Fazer um método que copia os elementos de uma lista para uma
-//    outra lista que pode ser mais genérica que a primeira.
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 	public static void main(String[] args) {
-		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
-		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
-		List<Object> myObjs = new ArrayList<Object>();
-		copy(myInts, myObjs);
-		printList(myObjs);
-		copy(myDoubles, myObjs);
-		printList(myObjs);
-	}
 
-	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
-		for (Number number : source) {
-			destiny.add(number);
-		}
-	}
+		Set<String> set = new LinkedHashSet<>();
 
-	public static void printList(List<?> list) {
-		for (Object obj : list) {
-			System.out.print(obj + " ");
+		set.add("TV");
+		set.add("Notebook");
+		set.add("Tablet");
+		set.add("Computador");
+		set.add("Celular");
+		set.add("Fone");
+
+		set.remove("Tablet");
+		set.removeIf(x -> x.length() <= 2); //Remove todos os itens com a quantia de caracteres menor/igual a 2.
+		set.removeIf(x -> x.charAt(0) == 'N');// Remove todos os itens que se inicia com o caracter 'N'.
+
+		for (String p : set) {
+			System.out.println(p);
 		}
+		
 		System.out.println();
+		
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
+		
+		// union
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println("União: "+ c);
+		
+		// intersection
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println("Intersecção: "+d);
+		
+		// difference
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println("Diferença: "+e);
 	}
 }
