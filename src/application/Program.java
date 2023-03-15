47dc5bbaa8d58b5b22143763e5c23657d6786692
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-14 21:56:16 -0300

    Exemplo de Invariantes

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,17 +4,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Program {
+//      Com tipos curinga podemos fazer métodos
+//		que recebem um genérico de "qualquer tipo":
+//      
+//		NOTA: Não é possível adicionar dados a uma
+//		coleção de tipo curinga.
 
 	public static void main(String[] args) {
-
 		List<Integer> myInts = Arrays.asList(5, 2, 10);
 		printList(myInts);
-		List<String> myStrs = Arrays.asList("Maria", "Bob", "Vini");
-		printList(myStrs);
-
 	}
 
 	public static void printList(List<?> list) {
+//		list.add(3); Erro de compilação  --> O compilador não sabe qual é o tipo específico do qual a lista foi instanciada.
 		for (Object obj : list) {
 			System.out.println(obj);
 		}
