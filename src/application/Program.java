695e9e7c9a1cbaa9f781d137be656f8c77729a35
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-16 09:22:11 -0300

    Exercício(Set)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,47 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeSet;
 
-import entities.Product_TreeSet;
+import entities.LogEntry;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Set<Product_TreeSet> set = new TreeSet<>();
-		set.add(new Product_TreeSet("TV", 900.0));
-		set.add(new Product_TreeSet("Notebook", 1200.0));
-		set.add(new Product_TreeSet("Tablet", 400.0));
-		for (Product_TreeSet p : set) {
-		System.out.println(p);
+
+		Scanner leia = new Scanner(System.in);
+
+		System.out.println("Enter file full path: ");
+		String path = leia.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			Set<LogEntry> set = new HashSet<>();
+
+			String line = br.readLine();
+			while (line != null) {
+
+				String[] fields = line.split(" ");
+				String username = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+
+				set.add(new LogEntry(username, moment));
+
+				line = br.readLine();
+			}
+
+			System.out.println("Total users: "+set.size());
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
+
+		leia.close();
 	}
 }
