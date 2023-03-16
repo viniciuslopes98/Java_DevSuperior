ab2d5bc9142f2a132317a22c9ee23dd34f684bb0
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-16 10:03:08 -0300

    Comparação de valores(Map)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,27 @@
 package application;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
+
+import entities.Product_map;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Map<String, String> cookies = new TreeMap<>();
+		Map<Product_map, Double> stock = new HashMap<>();
 		
-		cookies.put("username", "Maria");
-		cookies.put("email","Maria@gmail.com");
-		cookies.put("phone","99711122");
+		Product_map p1 = new Product_map("Tv", 900.0);
+		Product_map p2 = new Product_map("Notebook", 1200.0);
+		Product_map p3 = new Product_map("Tablet", 400.0);
 		
-		cookies.remove("email");
-		cookies.put("phone", "99771133");
+		stock.put(p1, 10000.0);
+		stock.put(p2, 20000.0);
+		stock.put(p3, 15000.0);
 		
-		System.out.println("Contains 'phone' key: "+cookies.containsKey("phone"));
-		System.out.println("Phone number: "+cookies.get("phone"));
-		System.out.println("Email: "+cookies.get("email"));
-		System.out.println("Size: "+cookies.size());
+		Product_map ps = new Product_map("Tv", 900.0);
 		
-		System.out.println("All cookies: ");
-		for(String key : cookies.keySet()) {
-			System.out.println(key + ": "+cookies.get(key));
-		}
+		System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
+	
 	}
 }
