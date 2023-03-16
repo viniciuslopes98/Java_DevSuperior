1af798f9420fd313d3c8f7fe40b1a8f5ffa7b1bf
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-16 09:55:46 -0300

    Introdução (Map)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,47 +1,29 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.time.Instant;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
-
-import entities.LogEntry;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Scanner leia = new Scanner(System.in);
-
-		System.out.println("Enter file full path: ");
-		String path = leia.nextLine();
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
-			Set<LogEntry> set = new HashSet<>();
-
-			String line = br.readLine();
-			while (line != null) {
-
-				String[] fields = line.split(" ");
-				String username = fields[0];
-				Date moment = Date.from(Instant.parse(fields[1]));
-
-				set.add(new LogEntry(username, moment));
-
-				line = br.readLine();
-			}
-
-			System.out.println("Total users: "+set.size());
-			
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+		Map<String, String> cookies = new TreeMap<>();
+		
+		cookies.put("username", "Maria");
+		cookies.put("email","Maria@gmail.com");
+		cookies.put("phone","99711122");
+		
+		cookies.remove("email");
+		cookies.put("phone", "99771133");
+		
+		System.out.println("Contains 'phone' key: "+cookies.containsKey("phone"));
+		System.out.println("Phone number: "+cookies.get("phone"));
+		System.out.println("Email: "+cookies.get("email"));
+		System.out.println("Size: "+cookies.size());
+		
+		System.out.println("All cookies: ");
+		for(String key : cookies.keySet()) {
+			System.out.println(key + ": "+cookies.get(key));
 		}
-
-		leia.close();
 	}
 }
