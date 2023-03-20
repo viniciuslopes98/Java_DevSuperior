9fed7795606516a01b79c63507d5cdb47f2d9d94
zm1hawk <vinicius6.lopes6@gmail.com>
2023-03-20 11:58:17 -0300

    Exemplo - Transparência referencial

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,45 +1,23 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
-import entities.Productt;
+import java.util.Arrays;
 
 public class Program {
+	
+	public static int globalValue = 3;
 
 	public static void main(String[] args) {
-
-		List<Productt> list = new ArrayList<>();
-
-		list.add(new Productt("TV", 900.00));
-		list.add(new Productt("Notebook", 1200.00));
-		list.add(new Productt("Tablet", 450.00));
-
-//		Outra forma de usar o comparator é declarando uma classe anônima:
-
-//		Comparator<Productt> comp = new Comparator<Productt>() {
-//
-//			@Override
-//			public int compare(Productt p1, Productt p2) {
-//				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-//			}
-//
-//		};
-
-//		Uma forma mais "enxuta" de usar o comparator é em Expressão Lambda 
-//		em função anônima(Arrow function):
 		
-//		Comparator<Productt> comp = (p1, p2) -> {
-//			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-//		};
+		int[] vect = new int[] { 3, 4, 5 };
+		changeOddValues(vect);
+		System.out.println(Arrays.toString(vect));
+	}
 
-//		Ainda é possível resumir mais, deixando da seguinte forma:
-		
-		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
-
-		for (Productt p : list) {
-			System.out.println(p);
+	public static void changeOddValues(int[] numbers) {
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] % 2 != 0) {
+				numbers[i] += globalValue;
+			}
 		}
 	}
 }
